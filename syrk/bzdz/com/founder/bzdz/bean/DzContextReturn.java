195ef67c871e89403ece5f08b150d48720db82f3
zhangguoliang <guoliang900417@163.com>
2015-12-03 10:44:01 +0800

    【新增】返回6段地址VO,服务接口使用

---
+++ syrk/bzdz/com/founder/bzdz/bean/DzContextReturn.java
@@ -0,0 +1,91 @@
+package com.founder.bzdz.bean;
+
+/**
+ * @类名: DzContextReturn
+ * @描述: 地址返回参数
+ * @作者: zhang_guoliang@founder.com
+ * @日期: 2015-11-30 下午15:00:21
+ */
+public class DzContextReturn {
+	private String mldzid;
+	private String chdzid;
+	private String mlpdm;
+	private String mlpxz;
+	private String dzid;
+	private String xzqhdm;
+	private String dzxz;
+	private String zbx;
+	private String zby;
+
+	public String getMldzid() {
+		return mldzid;
+	}
+
+	public void setMldzid(String mldzid) {
+		this.mldzid = mldzid;
+	}
+
+	public String getChdzid() {
+		return chdzid;
+	}
+
+	public void setChdzid(String chdzid) {
+		this.chdzid = chdzid;
+	}
+
+	public String getMlpdm() {
+		return mlpdm;
+	}
+
+	public void setMlpdm(String mlpdm) {
+		this.mlpdm = mlpdm;
+	}
+
+	public String getMlpxz() {
+		return mlpxz;
+	}
+
+	public void setMlpxz(String mlpxz) {
+		this.mlpxz = mlpxz;
+	}
+
+	public String getDzid() {
+		return dzid;
+	}
+
+	public void setDzid(String dzid) {
+		this.dzid = dzid;
+	}
+
+	public String getXzqhdm() {
+		return xzqhdm;
+	}
+
+	public void setXzqhdm(String xzqhdm) {
+		this.xzqhdm = xzqhdm;
+	}
+
+	public String getDzxz() {
+		return dzxz;
+	}
+
+	public void setDzxz(String dzxz) {
+		this.dzxz = dzxz;
+	}
+
+	public String getZbx() {
+		return zbx;
+	}
+
+	public void setZbx(String zbx) {
+		this.zbx = zbx;
+	}
+
+	public String getZby() {
+		return zby;
+	}
+
+	public void setZby(String zby) {
+		this.zby = zby;
+	}
+}
