a6cd036645e99fd35b2d16c227f1cbc4fdc5857b
zhangguoliang <guoliang900417@163.com>
2015-12-03 10:44:32 +0800

    【新增】移动警务VO,服务接口使用

---
+++ syrk/sqjw/com/founder/sqjw/vo/YdjwCountVO.java
@@ -0,0 +1,55 @@
+package com.founder.sqjw.vo;
+
+/**
+ * @类名: YdjwCountVO
+ * @描述: 移动警务统计VO
+ * @作者: zhang_guoliang@founder.com
+ * @日期: 2015-11-25 下午2:52:32
+ */
+public class YdjwCountVO {
+	private String bzdzsl;
+	private String syjzwsl;
+	private String syrksl;
+	private String syfwsl;
+	private String sydwsl;
+
+	public String getBzdzsl() {
+		return bzdzsl;
+	}
+
+	public void setBzdzsl(String bzdzsl) {
+		this.bzdzsl = bzdzsl;
+	}
+
+	public String getSyjzwsl() {
+		return syjzwsl;
+	}
+
+	public void setSyjzwsl(String syjzwsl) {
+		this.syjzwsl = syjzwsl;
+	}
+
+	public String getSyrksl() {
+		return syrksl;
+	}
+
+	public void setSyrksl(String syrksl) {
+		this.syrksl = syrksl;
+	}
+
+	public String getSyfwsl() {
+		return syfwsl;
+	}
+
+	public void setSyfwsl(String syfwsl) {
+		this.syfwsl = syfwsl;
+	}
+
+	public String getSydwsl() {
+		return sydwsl;
+	}
+
+	public void setSydwsl(String sydwsl) {
+		this.sydwsl = sydwsl;
+	}
+}
