7d63b90a33d2451d4fdcfc8c452989f04f0c3c61
congrihong <cong_rihong@founder.com.cn>
2015-12-15 15:07:35 +0800

    动态纪实涉毒贩毒功能开发

--- syrk/zdrygl/com/founder/zdrygl/dao/ZdryDtjsSdfdxxbDao.java
+++ syrk/zdrygl/com/founder/zdrygl/dao/ZdryDtjsSdfdxxbDao.java
@@ -7,42 +7,42 @@ import org.springframework.stereotype.Repository;
 
 import com.founder.framework.base.dao.BaseDaoImpl;
 import com.founder.framework.utils.EasyUIPage;
-import com.founder.zdrygl.bean.ZdryDtjsSfxxb;
+import com.founder.zdrygl.bean.ZdryDtjsSdfdxxb;
 
 /**
  * ****************************************************************************
- * @Package:      [com.founder.qbld.dao.ZdryDtjsSfxxbDao.java]  
- * @ClassName:    [ZdryDtjsSfxxbDao]   
- * @Description:  [动态纪实 上访信息]   
+ * @Package:      [com.founder.qbld.dao.ZdryDtjsSdfdxxb.java]  
+ * @ClassName:    [ZdryDtjsSdfdxxb]   
+ * @Description:  [动态纪实 涉毒贩毒信息]   
  * @Author:       [cong_rihong@founder.com.cn]  
- * @CreateDate:   [2015-12-9 上午9:30:05]   
+ * @CreateDate:   [2015-12-15 上午9:30:05]   
  * @UpdateUser:   [founder(如多次修改保留历史记录，增加修改记录)]   
- * @UpdateDate:   [2015-12-9 上午9:30:05，(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateDate:   [2015-12-15 上午9:30:05，(如多次修改保留历史记录，增加修改记录)]   
  * @UpdateRemark: [说明本次修改内容,(如多次修改保留历史记录，增加修改记录)]  
  * @Version:      [v1.0]
  */
-@Repository("zdryDtjsSfxxbDao") 
-public class ZdryDtjsSfxxbDao extends BaseDaoImpl {
+@Repository("zdryDtjsSdfdxxb") 
+public class ZdryDtjsSdfdxxbDao extends BaseDaoImpl {
 
 	
-	public void save(ZdryDtjsSfxxb entity) {
-		insert("ZdryDtjsSfxxb.save", entity); 
+	public void save(ZdryDtjsSdfdxxb entity) {
+		insert("ZdryDtjsSdfdxxb.save", entity); 
 	
 	}
 	
-	public String update(ZdryDtjsSfxxb entity) {
-		update("ZdryDtjsSfxxb.update", entity); 
+	public String update(ZdryDtjsSdfdxxb entity) {
+		update("ZdryDtjsSdfdxxb.update", entity); 
 		return entity.getId();
 	}
 	
-	public ZdryDtjsSfxxb querById(String id){
-		return (ZdryDtjsSfxxb)queryForObject("ZdryDtjsSfxxb.queryById", id);
+	public ZdryDtjsSdfdxxb querById(String id){
+		return (ZdryDtjsSdfdxxb)queryForObject("ZdryDtjsSdfdxxb.queryById", id);
 	}
 	
 
 	
-	public void delete(ZdryDtjsSfxxb entity){
-		update("ZdryDtjsSfxxb.delete", entity); 
+	public void delete(ZdryDtjsSdfdxxb entity){
+		update("ZdryDtjsSdfdxxb.delete", entity); 
 
 	}
 	
@@ -57,7 +57,7 @@ public class ZdryDtjsSfxxbDao extends BaseDaoImpl {
 	 * @throws
 	 */
 	public List<?> queryList(EasyUIPage page, Map<String, Object> map) {
-		return queryForList("ZdryDtjsSfxxb.queryByZdryZjhm", map);
+		return queryForList("ZdryDtjsSdfdxxb.queryByZdryZjhm", map);
 	}
 
 	
@@ -70,7 +70,7 @@ public class ZdryDtjsSfxxbDao extends BaseDaoImpl {
 	 * @throws
 	 */
 	public long queryCount(Map<String, Object> map) {
-		Integer count = (Integer) queryForObject("ZdryDtjsSfxxb.queryCountByZdryZjhm", map);
+		Integer count = (Integer) queryForObject("ZdryDtjsSdfdxxb.queryCountByZdryZjhm", map);
 		return count.longValue();
 	}	
 	
