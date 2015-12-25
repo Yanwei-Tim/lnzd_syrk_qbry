0bce41a737dbfbe7c1d444f2e3653483a8cd34d9
zhang.hai <zhang.hai@founder.com.cn>
2015-12-25 10:59:58 +0800

    添加情报人员 业务操作表 和相关的bean,dao,service
    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

--- syrk/qbry/com/founder/zdrygl/base/service/ZdryQbxxbService.java
+++ syrk/qbry/com/founder/zdrygl/base/service/ZdryQbxxbService.java
@@ -6,7 +6,19 @@ import org.springframework.stereotype.Service;
 import com.founder.zdrygl.base.dao.ZdryQbxxbDao;
 import com.founder.zdrygl.base.model.ZdryQbxxb;
 
-@Service("ZdryQbxxbService")
+/**
+ * ****************************************************************************
+ * @Package:      [com.founder.zdrygl.base.service.ZdryQbxxbService.java]  
+ * @ClassName:    [ZdryQbxxbService]   
+ * @Description:  [情报人员服务类]   
+ * @Author:       [zhang.hai@founder.com.cn]  
+ * @CreateDate:   [2015年12月25日 上午10:58:50]   
+ * @UpdateUser:   [ZhangHai(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateDate:   [2015年12月25日 上午10:58:50，(如多次修改保留历史记录，增加修改记录)]   
+ * @UpdateRemark: [说明本次修改内容,(如多次修改保留历史记录，增加修改记录)]  
+ * @Version:      [v1.0]
+ */
+@Service("zdryQbxxbService")
 public class ZdryQbxxbService {
 	
 	@Autowired
