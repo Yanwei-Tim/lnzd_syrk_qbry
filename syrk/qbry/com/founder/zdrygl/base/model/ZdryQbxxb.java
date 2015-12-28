d22cce2313002d27100cc8c4417a3ed84f10df65
zhang.hai <zhang.hai@founder.com.cn>
2015-12-28 11:31:25 +0800

    实有人口新增，由于全国库连不上，暂时改为复用本地人员基本信息表的人员信息
    Signed-off-by: zhang.hai <zhang.hai@founder.com.cn>

--- syrk/qbry/com/founder/zdrygl/base/model/ZdryQbxxb.java
+++ syrk/qbry/com/founder/zdrygl/base/model/ZdryQbxxb.java
@@ -1,9 +1,11 @@
 package com.founder.zdrygl.base.model;
 
 import java.io.Serializable;
+
 import com.founder.framework.annotation.DBInfoAnnotation;
 import com.founder.framework.annotation.FieldDesc;
 import com.founder.framework.base.entity.BaseEntity;
+import com.founder.zdrygl.core.model.Zdry;
 
 /**
  * ****************************************************************************
@@ -18,7 +20,7 @@ import com.founder.framework.base.entity.BaseEntity;
  * @Version:      [v1.0]
  */
 @DBInfoAnnotation(tableName = "ZDRY_QBRYXXB", pk = "ID")
-public class ZdryQbxxb extends BaseEntity implements Serializable {
+public class ZdryQbxxb extends BaseEntity implements Serializable, Zdry {
 
 	private static final long serialVersionUID = 1L;
 
